586cc8541c8dd0c559122c238ca1766a47e7e80b
Timothy <Timothy.L9915@outlook.com>
2026-02-28 18:05:06 -0800

    Created function to get student grades fro CVS file

--- data.py
+++ data.py
@@ -1,1 +1,12 @@
 def get_student_grades():
+    """Gets student grades from CVS File"""
+    student_grades = []
+
+    file = open("data.csv", "r")
+    for line in file:
+        clean = line.strip()
+        student_grades.append(clean)
+    file.close()
+
+    return student_grades
+
